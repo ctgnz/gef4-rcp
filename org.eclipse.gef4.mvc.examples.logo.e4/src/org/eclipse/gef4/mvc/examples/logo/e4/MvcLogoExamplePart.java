a9e8fd6c5f5c758b8f12de456e5864c62ba93784
ctgnz <mote@ctg.co.nz>
2016-04-16 11:47:26 +1200

    Added simple bundle to act as e4 example

---
+++ org.eclipse.gef4.mvc.examples.logo.e4/src/org/eclipse/gef4/mvc/examples/logo/e4/MvcLogoExamplePart.java
@@ -0,0 +1,51 @@
+
+package org.eclipse.gef4.mvc.examples.logo.e4;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.gef4.fx.nodes.InfiniteCanvas;
+import org.eclipse.gef4.mvc.examples.logo.MvcLogoExampleModule;
+import org.eclipse.gef4.mvc.examples.logo.model.FXGeometricModel;
+import org.eclipse.gef4.mvc.fx.domain.FXDomain;
+import org.eclipse.gef4.mvc.fx.viewer.FXViewer;
+import org.eclipse.gef4.mvc.models.ContentModel;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import javafx.scene.layout.BorderPane;
+
+public class MvcLogoExamplePart {
+  @Inject
+  public MvcLogoExamplePart() {
+
+  }
+
+  @PostConstruct
+  public void postConstruct(BorderPane container) {
+    Injector injector = Guice.createInjector(new MvcLogoExampleModule());
+    FXDomain domain = injector.getInstance(FXDomain.class);
+    FXViewer viewer = domain.getAdapter(FXViewer.class);
+    InfiniteCanvas canvas = viewer.getCanvas();
+    container.setCenter(canvas);
+    canvas.sceneProperty().addListener((observable, oldValue, newValue) -> {
+      if (canvas.getScene() != null) {
+        domain.activate();
+        try {
+          viewer.getAdapter(ContentModel.class).getContents().setAll(createContents());
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  public static List<FXGeometricModel> createContents() {
+    return Collections.singletonList(new FXGeometricModel());
+  }
+
+}
